a2ad4731ce54cc3b943ec1506a83ea8cc96c078e
robmanganelly <robmanganelly@gmail.com>
2021-11-22 23:06:18 -0500

    creating solution component (only layout)

--- src/app/solution/solution.component.ts
+++ src/app/solution/solution.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-solution',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolutionComponent implements OnInit {
 
+  formComment: FormGroup = new FormGroup({
+    'comment': new FormControl()
+  })
+
+  comments: number[] = [1,1,11,1,1,11,1,11,1,1,11]
+
+  myComment: string = 'leave a comment here'
+
+  favorite: boolean = false;
+  like: boolean = false;
+  dislike: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clickEdit():void{
+    alert('must navigate to create solution component: fake --must be implemented--') // todo implement
+  }
+
+  clickCopy():void{ //todo
+    alert('content copied: fake --must be implemented--')
+
+  }
+  clickFavorite():void{
+    this.favorite = !this.favorite;
+    if (this.favorite) {this.dislike = false;}
+  }
+  clickLike():void{
+    this.like = !this.like;
+    if(this.like){this.dislike = false;}
+  }
+  clickDislike():void{
+    this.dislike = !this.dislike;
+    if(this.dislike){
+      this.like = false;
+      this.favorite = false;
+    }
+
+  }
+
+  postComment():void{
+    //todo place logic
   }
 
 }
