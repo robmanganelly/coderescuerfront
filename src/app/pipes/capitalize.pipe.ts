8aa3236ca298c6476ed8b6f14204e4679ce616b1
robmanganelly <robmanganelly@gmail.com>
2021-12-25 15:34:56 -0500

    fixing bugs

--- src/app/pipes/capitalize.pipe.ts
+++ src/app/pipes/capitalize.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CapitalizePipe implements PipeTransform {
 
-  transform(value: string, ...args: unknown[]): string {
-    return value.charAt(0).toUpperCase() + value.slice(1);  ;
+  transform(value?: string, ...args: unknown[]): string {
+    return !!value? value.charAt(0).toUpperCase() + value.slice(1) : '';  ;
   }
 
 }
